fd483b5c950ddba2a5aecd027f48340f0d24b6ff
unknown <kashif.umair@oztechwork.com>
2025-02-27 16:20:49 +0500

    route issue

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Routes>
 
         <Route index path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/About-us" element={<AboutUs />} />
         <Route path="/ourServices" element={<OurServices />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact-us" element={<ContactPage />} />
